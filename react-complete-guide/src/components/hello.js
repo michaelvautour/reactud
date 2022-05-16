f8847b6c33ab7775d15ccc7d6df1f1ab2844e2a1
Michael Vautour <michael.e.vautour@gmail.com>
2022-05-15 20:10:13 -0600

    Added typescript functionality (tsconfig.json, test files)

---
+++ react-complete-guide/src/components/hello.js
@@ -0,0 +1,6 @@
+"use strict";
+let firstName = "Hello";
+console.log(firstName);
+console.log(typeof (firstName));
+console.log("manually compile");
+//# sourceMappingURL=hello.js.map
---
+++ react-complete-guide/src/components/hello.ts
@@ -0,0 +1,5 @@
+let firstName: string = "Hello";
+
+console.log(firstName);
+console.log(typeof(firstName));
+console.log("manually compile");
